eff6c7c113b6edb3d79557c038292a0017be8dbf
Nokaubure <ivansoto.80@gmail.com>
2025-07-21 21:26:31 +0200

    better prefix flag

--- src/z64make.c
+++ src/z64make.c
@@ -393,7 +393,7 @@ static ThreadFunc Object_Convert(const char* path) {
 		"--out %s "
 		"--address 0x%08X "
 		"--scale %g "
-		"--prefixes %s "
+		"%s"
 		"--header %s "
 		"--linker %s"
 		,
@@ -402,7 +402,7 @@ static ThreadFunc Object_Convert(const char* path) {
 		out,
 		(Ini_GetInt(&mem, "segment") << 24),
 		Ini_GetFloat(&mem, "scale"),
-		prefixes,
+		!prefixes ? "--noprefixes " : "",
 		header,
 		linker
 	);
