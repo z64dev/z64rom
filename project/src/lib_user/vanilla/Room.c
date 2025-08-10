38d67ced1a75c1c84289489c9add4fa6bc845bdc
Zeldaboy14 <zeldaboy1999@gmail.com>
2025-08-10 02:56:41 -0500

    Add MM's culling
    We do this because MM does a few things different from OOT. See this for more info: https://blog.zeldaboy14productionworks.com/2025/08/room-culling-woes.html

--- project/src/lib_user/vanilla/Room.c
+++ project/src/lib_user/vanilla/Room.c
@@ -142,3 +142,224 @@ s32 func_8009728C(PlayState* play, RoomContext* roomCtx, s32 roomNum) {
     
     return 0;
 }
+
+Asm_VanillaHook(Room_DrawCullable);
+void Room_DrawCullable(PlayState* play, Room* room, u32 flags) {
+    RoomShapeCullable* roomShape;
+    RoomShapeCullableEntry* roomShapeCullableEntry;
+    RoomShapeCullableEntryLinked linkedEntriesBuffer[ROOM_SHAPE_CULLABLE_MAX_ENTRIES];
+    RoomShapeCullableEntryLinked* head = NULL;
+    RoomShapeCullableEntryLinked* tail = NULL;
+    RoomShapeCullableEntryLinked* iter;
+    Gfx* displayList;
+    RoomShapeCullableEntryLinked* insert;
+    s32 j;
+    s32 i;
+    Vec3f pos;
+    Vec3f projectedPos;
+    f32 projectedW;
+    RoomShapeCullableEntry* roomShapeCullableEntries;
+    RoomShapeCullableEntry* roomShapeCullableEntryIter;
+    f32 entryBoundsNearZ;
+
+    OPEN_DISPS(play->state.gfxCtx, "../z_room.c", 287);
+	
+    if (flags & ROOM_DRAW_OPA) {
+        func_800342EC(&D_801270A0, play);
+
+        gSPSegment(POLY_OPA_DISP++, 0x03, room->segment);
+        func_80093C80(&play->state);
+        gSPMatrix(POLY_OPA_DISP++, &gMtxClear, G_MTX_MODELVIEW | G_MTX_LOAD);
+    }
+
+    if (flags & ROOM_DRAW_XLU) {
+        func_800342EC(&D_801270A0, play);
+        gSPSegment(POLY_XLU_DISP++, 0x03, room->segment);
+        Gfx_SetupDL_25Xlu(play->state.gfxCtx);
+        gSPMatrix(POLY_XLU_DISP++, &gMtxClear, G_MTX_MODELVIEW | G_MTX_LOAD);
+    }
+
+    roomShape = &room->roomShape->cullable;
+    roomShapeCullableEntry = SEGMENTED_TO_VIRTUAL(roomShape->entries);
+    insert = linkedEntriesBuffer;
+
+    roomShapeCullableEntries = roomShapeCullableEntry;
+	
+    if (play->roomCtx.status < 0) {
+		// has no effect in oot. the above is faked, but keeping this for the if else statment. does actually build
+        /* (i = 0; i < roomShape->numEntries; i++, roomShapeCullableEntry++) {
+            if (R_ROOM_CULL_DEBUG_MODE != 0) {
+                if (((R_ROOM_CULL_DEBUG_MODE == ROOM_CULL_DEBUG_MODE_UP_TO_TARGET) &&
+                     (i <= R_ROOM_CULL_DEBUG_TARGET)) ||
+                    ((R_ROOM_CULL_DEBUG_MODE == ROOM_CULL_DEBUG_MODE_ONLY_TARGET) && (i == R_ROOM_CULL_DEBUG_TARGET))) {
+                    if (flags & ROOM_DRAW_OPA) {
+                        displayList = roomShapeCullableEntry->opa;
+                        if (displayList != NULL) {
+                            gSPDisplayList(POLY_OPA_DISP++, displayList);
+                        }
+                    }
+
+                    if (flags & ROOM_DRAW_XLU) {
+                        displayList = roomShapeCullableEntry->xlu;
+                        if (displayList != NULL) {
+                            gSPDisplayList(POLY_XLU_DISP++, displayList);
+                        }
+                    }
+                }
+            } else {
+                if (flags & ROOM_DRAW_OPA) {
+                    displayList = roomShapeCullableEntry->opa;
+                    if (displayList != NULL) {
+                        gSPDisplayList(POLY_OPA_DISP++, displayList);
+                    }
+                }
+
+                if (flags & ROOM_DRAW_XLU) {
+                    displayList = roomShapeCullableEntry->xlu;
+                    if (displayList != NULL) {
+                        gSPDisplayList(POLY_XLU_DISP++, displayList);
+                    }
+                }
+            }
+        }*/
+    } else {	
+	f32 var_fa1 = 1.0f; // play->billboardMtx;
+    f32 var_fv1;
+    s32 var_a1;
+
+    // Pick and sort entries by depth
+    for (i = 0; i < roomShape->numEntries; i++, roomShapeCullableEntry++) {
+
+        // Project the entry position, to get the depth it is at.
+        pos.x = roomShapeCullableEntry->boundsSphereCenter.x;
+        pos.y = roomShapeCullableEntry->boundsSphereCenter.y;
+        pos.z = roomShapeCullableEntry->boundsSphereCenter.z;
+        SkinMatrix_Vec3fMtxFMultXYZW(&play->viewProjectionMtxF, &pos, &projectedPos, &projectedW);
+		
+        projectedPos.z *= var_fa1;
+
+        var_fv1 = ABS_ALT(roomShapeCullableEntry->boundsSphereRadius);
+
+        // If the entry bounding sphere isn't fully before the rendered depth range
+        if (-var_fv1 < projectedPos.z) {
+
+            // Compute the depth of the nearest point in the entry's bounding sphere
+            entryBoundsNearZ = projectedPos.z - roomShapeCullableEntry->boundsSphereRadius;
+
+            // If the entry bounding sphere isn't fully beyond the rendered depth range
+            if (entryBoundsNearZ < play->lightCtx.fogFar) {
+
+                // This entry will be rendered
+                insert->entry = roomShapeCullableEntry;
+                //insert->boundsNearZ = entryBoundsNearZ;
+				
+                if (roomShapeCullableEntry->boundsSphereRadius < 0) {
+                    insert->boundsNearZ = FLT_MAX;
+                } else {
+                    insert->boundsNearZ = entryBoundsNearZ;
+                }
+
+                // Insert into the linked list, ordered by ascending depth of the nearest point in the bounding sphere
+                iter = head;
+                if (iter == NULL) {
+                    head = tail = insert;
+                    insert->prev = insert->next = NULL;
+                } else {
+                    do {
+                        if (insert->boundsNearZ < iter->boundsNearZ) {
+                            break;
+                        }
+                        iter = iter->next;
+                    } while (iter != NULL);
+
+                    if (iter == NULL) {
+                        insert->prev = tail;
+                        insert->next = NULL;
+                        tail->next = insert;
+                        tail = insert;
+                    } else {
+                        insert->prev = iter->prev;
+                        if (insert->prev == NULL) {
+                            head = insert;
+                        } else {
+                            insert->prev->next = insert;
+                        }
+                        iter->prev = insert;
+                        insert->next = iter;
+                    }
+                }
+
+                insert++;
+            }
+        }
+    }
+
+    // if this is real then I might not be
+    R_ROOM_CULL_NUM_ENTRIES = roomShape->numEntries & 0xFFFF & 0xFFFF & 0xFFFF;
+	
+    // Draw entries, from nearest to furthest
+    i = 1;
+
+        if (flags & ROOM_DRAW_OPA) {
+            for (; head != NULL; head = head->next, i++) {
+//                s32 pad;
+
+                roomShapeCullableEntry = head->entry;
+
+                if (R_ROOM_CULL_DEBUG_MODE != ROOM_CULL_DEBUG_MODE_OFF) {
+                    // Debug mode drawing
+
+                    if (((R_ROOM_CULL_DEBUG_MODE == ROOM_CULL_DEBUG_MODE_UP_TO_TARGET) &&
+                         (i <= R_ROOM_CULL_DEBUG_TARGET)) ||
+                        ((R_ROOM_CULL_DEBUG_MODE == ROOM_CULL_DEBUG_MODE_ONLY_TARGET) &&
+                         (i == R_ROOM_CULL_DEBUG_TARGET))) {
+
+                        displayList = roomShapeCullableEntry->opa;
+                        if (displayList != NULL) {
+                            gSPDisplayList(POLY_OPA_DISP++, displayList);
+                        }
+                    }
+                } else {
+                    displayList = roomShapeCullableEntry->opa;
+                    if (displayList != NULL) {
+                        gSPDisplayList(POLY_OPA_DISP++, displayList);
+                    }
+                }
+            }
+        }
+
+        if (flags & ROOM_DRAW_XLU) {
+            for (; tail != NULL; tail = tail->prev) {
+                f32 temp_fv0;
+                f32 temp_fv1;
+
+                roomShapeCullableEntry = tail->entry;
+                displayList = roomShapeCullableEntry->xlu;
+
+                if (displayList != NULL) {
+                    if (roomShapeCullableEntry->boundsSphereRadius & 1) {
+
+                        temp_fv0 = tail->boundsNearZ - (f32)(iREG(93) + 0xBB8);
+                        temp_fv1 = iREG(94) + 0x7D0;
+
+                        if (temp_fv0 < temp_fv1) {
+                            if (temp_fv0 < 0.0f) {
+                                var_a1 = 255;
+                            } else {
+                                var_a1 = 255 - (s32)((temp_fv0 / temp_fv1) * 255.0f);
+                            }
+                            gDPSetEnvColor(POLY_XLU_DISP++, 255, 255, 255, var_a1);
+                            gSPDisplayList(POLY_XLU_DISP++, displayList);
+                        }
+                    } else {
+                        gSPDisplayList(POLY_XLU_DISP++, displayList);
+                    }
+                }
+            }
+        }
+
+        R_ROOM_CULL_USED_ENTRIES = i - 1;
+    }
+
+    CLOSE_DISPS(play->state.gfxCtx, "../z_room.c", 430);
+}
