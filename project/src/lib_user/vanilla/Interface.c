550c8f37d427e1009337aa981891abff72c41a0a
Zeldaboy14 <zeldaboy1999@gmail.com>
2025-09-29 17:15:15 -0500

    update interface again
    this got undone by complete accident. i gotta stop causing bugs!

--- project/src/lib_user/vanilla/Interface.c
+++ project/src/lib_user/vanilla/Interface.c
@@ -576,7 +576,11 @@ Gfx* Gfx_DrawTexQuadIA8(Gfx* gfx, void* texture, s16 textureWidth, s16 textureHe
 		
 Asm_VanillaHook(Interface_DrawItemButtons);
 void Interface_DrawItemButtons(PlayState* play) {
+	#if Patch_INTERFACE_C_UP_TATL == true
     static void* cUpLabelTextures[] = { gNaviCUpJPTex, gNaviCUpJPTex, gNaviCUpJPTex };
+	#elif Patch_INTERFACE_C_UP_TATL == false
+	static void* cUpLabelTextures[] = { gNaviCUpENGTex, gNaviCUpENGTex, gNaviCUpENGTex };
+	#endif
     static s16 startButtonLeftPos[] = { 132, 130, 130 };
     InterfaceContext* interfaceCtx = &play->interfaceCtx;
     Player* player = GET_PLAYER(play);
