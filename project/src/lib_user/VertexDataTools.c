54b16c2adc45cd90d4bec1e0b2c812ececf777a3
Zeldaboy14 <zeldaboy1999@gmail.com>
2025-12-30 22:06:29 -0600

    add vertexdatatools
    new file for rotating water stuff. forgotten commit

---
+++ project/src/lib_user/VertexDataTools.c
@@ -0,0 +1,41 @@
+#include <z64hdr.h>
+#include "uLib.h"
+
+void UvRotate(PlayState *play, UvRotateConfig *config)
+{
+    u32 gameplayFrames = play->gameplayFrames;
+    
+    if (!config->data)
+    {
+        config->data = SEGMENTED_TO_VIRTUAL(config->segAddr);
+        
+        for (int i = 0; i < config->numVerts; ++i)
+            memcpy(&config->uvData[i], config->data[i].v.tc, sizeof(Vec2s));
+    }
+    
+    for (int i = 0; i < config->numVerts; ++i)
+    {
+        const Vec2s *src = &config->uvData[i];
+              Vec2s *dst = (Vec2s*)(config->data[i].v.tc);
+
+        // Angle advances every frame
+        s16 angle = gameplayFrames * config->speed;
+
+        // OoT trig returns values in s16 fixed-point (1.0 = 0x7FFF)
+        s16 sinA = sins(angle);
+        s16 cosA = coss(angle);
+
+        // Original texture coordinates
+        s32 u = src->x;
+        s32 v = src->y;
+
+        // Clockwise rotation around (0, 0)
+        // u' =  u*cosθ + v*sinθ
+        // v' = -u*sinθ + v*cosθ
+        s32 uRot = ( u * cosA + v * sinA) >> 15;
+        s32 vRot = (-u * sinA + v * cosA) >> 15;
+
+        dst->x = (s16)uRot;
+        dst->y = (s16)vRot;
+    }
+}
